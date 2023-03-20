2fba696179aafdafe3dfa2d7fd65f606903a7101
Omer Alon <omer.a.alon@gmail.com>
2023-03-21 00:24:18 +0200

    Update copy

--- js/data/features.js
+++ js/data/features.js
@@ -4,31 +4,31 @@ const featureData = {
     {
         icon: "fas fa-copyright fa-2x",
         title: 'Tailored Service',
-        description: 'We’ll work with you to build customized automations that work exactly how YOU want them to',
+        description: 'We’ll work with you to build customized AI-powered automations that work exactly how YOU want them to',
         active: true,
         background: 'FFF6D3' ,
         color: 'D6AC02'
     },
     {
         icon: "fas fa-photo-video fa-2x",
-        title: 'Don\'t be in the dark',
-        description: 'Get a full walkthrough and documentation of every automation implemented',
+        title: 'Cultivate Predictability',
+        description: 'Accurately predict customer needs and provide them with the support they need in a timely manner',
         active: true,
         background: 'F1EBFE',
         color: '794ADE'
     }, 
     {
         icon: "fas fa-brain fa-2x",
-        title: 'Work Smart',
-        description: 'We implement automations for any task in your business— from onboarding customers to team workflows',
+        title: 'Increase Scaleability',
+        description: 'As your operations grow, you\'ll be able to easily handle the increased traffic',
         active: true,
         background: 'E4FFEE',
         color: '40975F'
     },
     {
         icon: "fas fa-cloud fa-2x ",
-        title: '360° Approach',
-        description: 'Our team has experience with almost every kind of automation software out there',
+        title: '24/7 Support',
+        description: 'Our chatbots can help you communicate with your customers around the clock, even when you\'re not available',
         active: true,
         background: 'EAF9FF' ,
         color: '45B1E1'
@@ -36,16 +36,16 @@ const featureData = {
     },
     {
         icon: "fas fa-cogs fa-2x",
-        title: 'Iterate & Optimize',
-        description: 'Automations need to be constantly refined & updated. We stick around to review and monitor each one',
+        title: 'Customer Communication',
+        description: 'They can handle routine tasks like answering frequently asked questions, scheduling appointments, and qualifying leads',
         active: true,
         background: 'FFECEF' ,
         color: 'FF4361'
     },
     {
         icon: "fas fa-headset fa-2x",
-        title: 'Full Support',
-        description: 'We work with you in every step of the way to connect the human side of your business to the technology side of it',
+        title: 'Analytics and Reporting',
+        description: 'Our chatbots can help you track and improve engagement, conversions, and other key metrics',
         active: true,
         background: 'EAF9FF' ,
         color: '54B5E2'
